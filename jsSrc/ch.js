129c3b2a7675a86feb6d0e527f6a603a49052a9b
SeN75 <saa111954@hotmail.com>
2017-04-15 20:55:40 +0300

    tabtab is number one

---
+++ jsSrc/ch.js
@@ -0,0 +1,24 @@
+function tl(){
+    
+    
+    switch (parseInt(document.getElementById("selectPart").value))
+            {
+         case 1:
+            window.location.href="phs.html";
+            
+            break;
+            
+            case 2:
+            window.location.href="phs.html";
+            
+            break;
+            
+            case 3:
+            window.location.href="phs.html";
+            
+            break;
+            
+            }
+    
+    
+}
